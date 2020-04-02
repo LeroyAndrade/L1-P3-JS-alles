84bb50002c31bed7415e9d74c1817ca510718528
LeroyAndrade <30168@ma-web.nl>
2020-04-02 16:37:19 +0200

    1-4
    1-4

--- opgave1/js/javaScript.js
+++ opgave1/js/javaScript.js
@@ -26,12 +26,45 @@ function OBJboek (titel,isbn,druk,jaar,auteur,type,korting){
   OBJBoek.Korting = korting;
 }
 let forIn2="";
-let boek = new OBJboek ("The Adventures of Tom Sawyer","ISBN-13: 978-0553211283","1", "1876", "Mark Twain", "Fictie", "10%");
+let boek = new OBJboek ("The Adventures of Tom Sawyer","ISBN-13: 978-0553211283","1", 1876, "Mark Twain", "Fictie", "10%");
 for (const property in boek) {
 forIn2+=(`${property}: ${boek[property]}`+'<br/>');
 }document.write('<br/>'+"Opgave 2" +'<br/>'+ '<form>'+'<td>'+forIn2+'</td>'+ '</form>');
 
 
-console.log(boek.Titel);
+//Opgave III
+function bluePrintVoertuig (merknaam,model,gewicht,motorinhoud,kleur){
+  let bluePrintVoertuig = this;
+  bluePrintVoertuig.Merknaam = merknaam;
+  bluePrintVoertuig.Model = model;
+  bluePrintVoertuig.Gewicht = gewicht;
+  bluePrintVoertuig.MotorInhoud = motorinhoud;
+  bluePrintVoertuig.Kleur = kleur;
+}
+
+let forIn3="";
+let voertuig1 = new bluePrintVoertuig("Ferrari", 'ENZO', '1,480 kg', '6L V12', 'Rood');
+for (const property in voertuig1) {
+forIn3+=(`${property}: ${voertuig1[property]}`+'<br/>');
+}document.write('<br/>'+"Opgave 3" +'<br>'+forIn3);
+//Opgave III.I
+let forIn31="";
+let voertuig2 = new bluePrintVoertuig("theFuture", 'Grande', '1,481 kg', '33L V12 + Efficiënt appart Hybrid en Energy Recovery System tijdens remmen', 'Groen');
+for (const property in voertuig2) {
+forIn31+=(`${property}: ${voertuig1[property]}`+'<br/>');
+}document.write('<br/>'+"Opgave 3.1" +'<br>'+forIn31);
+
+
+//Opgave IIII
+
+//let boek = new OBJBoek ("");
+
+
+
+
+
+
+
+
 
 //let boek = new OBJBoek ("");
